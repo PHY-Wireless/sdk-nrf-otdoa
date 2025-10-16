b9d88cb501ee9fdff9e940966612285551e4f87f
Zack Mudd <97700700+serranozack@users.noreply.github.com>
2025-10-16 11:54:46 -0700

    [PHYW-527] Libraries in CMake (#14)
    * otdoa: handle library import in global cmake
    
    Pulls in OTDOA library from ext/lib/bin/otdoa
    
    Signed-off-by: Zack Mudd <zmudd@serranosystems.com>
    
    * otdoa: control filesystem settings through Kconfig
    
    Replaces LittleFS and External Storage options in
    per-project CMakeLists.txt files
    
    Signed-off-by: Zack Mudd <zmudd@serranosystems.com>
    
    * otdoa: support linking dev build of OTDOA
    
    aligns Kconfig with changes in OTDOA library
    
    Signed-off-by: Zack Mudd <zmudd@serranosystems.com>
    
    * otdoa: fix failing compliance check
    
    Compliance failed because of incorrect leading
    whitespace in OTDOA AL Kconfig
    
    Signed-off-by: Zack Mudd <zmudd@serranosystems.com>
    
    * otdoa: update overview doc
    
    Replaces reference to CONFIG_OTDOA_DEBUG_LOGS with list of OTDOA
    library type Kconfig choices
    
    Signed-off-by: Zack Mudd <zmudd@serranosystems.com>
    
    * otdoa: fix warning when building sample
    
    Compile gate in the littlefs header had the conditional
    the wrong way around.
    
    Signed-off-by: Zack Mudd <zmudd@serranosystems.com>
    
    * otdoa: move otdoa_little_fs.c from sample to al
    
    draft commit, there's an include issue with lfs.h
    
    Signed-off-by: Zack Mudd <zmudd@serranosystems.com>
    
    * lib: otdoa_al:  Fix issue with littlefs module include path
    
    Explicitly added modules/fs/littlefs to the include path.
    
    Signed-off-by: Neal Riedel <nriedel@serranosystems.com>
    
    * lib: otdoa_al: Fix little FS compilation issue
    
    Conditionally add otdoa_little_fs.c to otdoa_al only when
    CONFIG_OTDOA_LITTLE_FS is set.
    
    Signed-off-by: Neal Riedel <nriedel@serranosystems.com>
    
    ---------
    
    Signed-off-by: Zack Mudd <zmudd@serranosystems.com>
    Signed-off-by: Neal Riedel <nriedel@serranosystems.com>
    Co-authored-by: Neal Riedel <nriedel@serranosystems.com>

--- include/otdoa_al/otdoa_little_fs.h
+++ include/otdoa_al/otdoa_little_fs.h
@@ -7,20 +7,14 @@
 #ifndef SRC_INCLUDE_OTDOA_FILE_H_
 #define SRC_INCLUDE_OTDOA_FILE_H_
 
-#ifndef OTDOA_LITTLE_FS
-#error "OTDOA_LITTLE_FS not defined"
-#endif
-
-#if OTDOA_LITTLE_FS == 0
-int mount_fat_fs(void);
-int test_fs_raw(void);
-int test_fs(void);
-
-#else
+#if CONFIG_OTDOA_LITTLE_FS
 int mount_little_fs(void);
 int unmount_little_fs(void);
 int test_little_fs(void);
-
+#else
+int mount_fat_fs(void);
+int test_fs_raw(void);
+int test_fs(void);
 #endif
 
 #endif /* SRC_INCLUDE_OTDOA_FILE_H_ */
