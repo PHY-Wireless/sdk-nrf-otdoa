16942cc09b4e515a858e84b9e1473a1c5ceaaf34
Neal Riedel <nriedel@serranosystems.com>
2025-12-12 10:20:00 -0800

    lib: otdoa_lib: Support 417 error code returned from server
    Modifies the HTTP error codes to include 417 Expectation Failed
    return code.  This code is returned by the server to indicate
    a failure in the processing of the JWT.  Previously a 500
    failure was returned by the server.
    
    Signed-off-by: Neal Riedel <nriedel@serranosystems.com>

--- include/otdoa_al/otdoa_api.h
+++ include/otdoa_al/otdoa_api.h
@@ -118,6 +118,11 @@ typedef enum {
 	/** User already has a pending uBSA */
 	OTDOA_EVENT_FAIL_HTTP_CONFLICT = 409,
 
+	/** Expectation Failed - server will return this on failure in SSL verify
+	 * UE will retry as a workaround
+	 */
+	USER_EVENT_FAIL_HTTP_EXPECT_FAIL = 417,
+
 	/** uBSA generation was not possible */
 	OTDOA_EVENT_FAIL_HTTP_UNPROCESSABLE_CONTENT = 422,
 
