834d1fee81b58d825a8591abf5f5d78e56d6f8eb
Zack Mudd <zmudd@serranosystems.com>
2026-06-01 18:04:41 -0700

    lib: otdoa_al: provide definitions for OTDOA timing macros
    used by the nordic_vectors target
    
    Signed-off-by: Zack Mudd <zmudd@serranosystems.com>

--- lib/otdoa_al/otdoa_timers.c
+++ lib/otdoa_al/otdoa_timers.c
@@ -5,6 +5,7 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/timing/timing.h>
 
 #include <stdint.h>
 #include <otdoa_al/otdoa_al2otdoa_api.h>
@@ -81,4 +82,69 @@ int otdoa_timer_active(unsigned int u_timer_no)
 		return 1;
 	}
 	return 0;
+}
+
+static timing_t start_time, stop_time;
+static uint64_t total_cycles;
+
+// Initialize the Timer & cycle counter
+void TST_initTimerAndCycleCnt(void)
+{
+	timing_init();
+	total_cycles = 0;
+}
+
+void TST_timerStart(void)
+{
+	timing_start();
+	start_time = timing_counter_get();
+}
+
+void TST_timerStop(void)
+{
+	stop_time = timing_counter_get();
+	timing_stop();
+	total_cycles += timing_cycles_get(&start_time, &stop_time);
+}
+
+// log the count of cycles
+void TST_logCycles( const char* const pszName, int16_t i16Count, const char* pszUnits)
+{
+	printk("Timer %s: %lld cycles (%d %s)\n", pszName, total_cycles, i16Count, pszUnits);
+}
+
+// Pause the timer and accumulate cycles
+void TST_pauseTimer(void)
+{
+	stop_time = timing_counter_get();
+	total_cycles += timing_cycles_get(&start_time, &stop_time);
+	start_time = stop_time;
+}
+
+static timing_t dwt_start_time, dwt_stop_time;
+static uint64_t dwt_total_cycles;
+
+// DWT timer functions
+void TST_InitDWT()
+{
+	timing_init();
+	dwt_total_cycles = 0;
+}
+
+void TST_DWTStart(void)
+{
+	timing_start();
+	dwt_start_time = timing_counter_get();
+}
+
+void TST_DWTStop(void)
+{
+	dwt_stop_time = timing_counter_get();
+	timing_stop();
+	dwt_total_cycles += timing_cycles_get(&dwt_start_time, &dwt_stop_time);
+}
+
+void TST_DWTlogCycles(const char *const pszName, int16_t i16Count, const char *pszUnits)
+{
+	printk("DWT %s: %lld cycles (%d %s)\n", pszName, dwt_total_cycles, i16Count, pszUnits);
 }
